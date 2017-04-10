68d11c5402ecf8ab23c800d3ffba251211c9c8ea
chengyafeng123 <1527273406@qq.com>
2017-04-10 14:34:32 +0800

    EEE

---
+++ CYQSCore/CYQSCore/CYQSCore.h
@@ -0,0 +1,13 @@
+//
+//  CYQSCore.h
+//  CYQSCore
+//
+//  Created by lashou456 on 2017/4/10.
+//  Copyright © 2017年 lashou456. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CYQSCore : NSObject
+
+@end
---
+++ CYQSCore/CYQSCore/CYQSCore.m
@@ -0,0 +1,13 @@
+//
+//  CYQSCore.m
+//  CYQSCore
+//
+//  Created by lashou456 on 2017/4/10.
+//  Copyright © 2017年 lashou456. All rights reserved.
+//
+
+#import "CYQSCore.h"
+
+@implementation CYQSCore
+
+@end
